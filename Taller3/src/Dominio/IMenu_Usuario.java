5888e23034be580f98736b60cd37d1f6a050df05
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2025-11-15 00:27:23 -0300

    Add files via upload
    cambio_del_15_11

---
+++ Taller3/src/Dominio/IMenu_Usuario.java
@@ -0,0 +1,12 @@
+package Dominio;
+
+public class IMenu_Usuario {
+	interface IMenuUsuario{
+		public void ver_proyectos_disponibles();
+		public void ver_tareas_asignadas();
+		public void actualizar_estado_tarea();
+		public void aplicar_visitor_sobre_tareas();
+		
+	}
+
+}
---
+++ Taller3/src/Dominio/JMenuAdministrar.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class JMenuAdministrar {
+	
+	interface Menu_Dministrador{
+		public void ver_listacompleta_proyectoy_tarea();
+		public void Agregar_eliminar_proyecto();
+		public void Agregar_Eliminar_proyecto();
+		public void Asignar_prioridad_Strategy();
+		public void Generar_reporte_proyecto();
+			
+		}
+	
+}
