97d55de4a1a8d82669b4952866d62741d90deb0c
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2025-11-16 21:48:34 -0300

    avance e implementación
    avance e implementación de herencia, pero problema con esto e imposibilidad de avanzar con el polimorfismo

---
+++ Taller3/src/Dominio/Administrador.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+
+
+public class Administrador extends Usuario_generico  {
+
+	public Administrador(String Username, String Contraseña, String Rol) {
+		super(Username, Contraseña, Rol);
+		// TODO Auto-generated constructor stub
+	}
+		}
+	
+
+
