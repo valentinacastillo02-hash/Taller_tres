d78fd2600be7f90fe8a77cd3d46206d94ee68a40
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2025-11-21 18:34:19 -0300

    Completar Bug

--- Dominio/Bug.java
+++ Dominio/Bug.java
@@ -2,12 +2,26 @@ package Dominio;
 //Alta Prioridad
 import java.time.LocalDate;
 
+import Patrones.PatronVisitorTarea;
+
 public class Bug extends Tarea{
 
 	public Bug(String iD_proyecto, String idTarea, String tipoTarea, String descripcionTarea, String estadoTarea,
 			String responsableTarea, String complejidadTarea, LocalDate fechaTarea) {
 		super(iD_proyecto, idTarea, tipoTarea, descripcionTarea, estadoTarea, responsableTarea, complejidadTarea,
 				fechaTarea);
+	}
+	@Override
+	public void aceptado(PatronVisitorTarea visitor) {
+		// TODO Auto-generated method stub
+		visitor.visitarBug(this);
+		
+	}
+
+	@Override
+	public int gaetImpacto() {
+		// TODO Auto-generated method stub
+		return 3;
 	}
 
 
